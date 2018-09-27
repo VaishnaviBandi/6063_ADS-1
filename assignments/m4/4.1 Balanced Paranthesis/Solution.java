bc21237511bd165d19b14a80dab0484740f4c38b
VaishnaviBandi <vaishnavibandi9@msitprogram.net>
2018-09-27 18:47:55 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -1,32 +1,51 @@
+/**
+ * { item_description }
+ */
 import java.util.Scanner;
+/**
+ * Class for linked stack of strings.
+ */
 class LinkedStackOfStrings {
 	private Node first = null;
 	private class Node {
-		char item;
+		char element;
 		Node nextAddress;
 	}
+	/**
+	 * { function_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public char top() {
-		return first.item;
+		return first.element;
 	}
-
-
+	/**
+	 * Determines if empty.
+	 *
+	 * @return     True if empty, False otherwise.
+	 */
 	public boolean isEmpty() {
 		return first == null;
 	}
-	public void push(char item) {
-		Node oldfirst = first;
+	/**
+	 * { function_description }
+	 *
+	 * @param      element  The element
+	 */
+	public void push(char element) {
+		Node old = first;
 		first = new Node();
-		first.item = item;
-		first.nextAddress = oldfirst;
+		first.element = element;
+		first.nextAddress = old;
 	}
 	public char pop() {
-		char item = first.item;
+		char element = first.element;
 		first = first.nextAddress;
-		return item;
+		return element;
 	}
 }
 public class Solution {
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int size = Integer.parseInt(sc.nextLine());
 		for (int i = 0; i < size; i++) {
@@ -39,7 +58,7 @@ public class Solution {
 
 		}
 	}
-	public static boolean checkParanthesis(String s) {
+	public static boolean checkParanthesis(final String s) {
 		LinkedStackOfStrings obj = new LinkedStackOfStrings();
 		int sLength = s.length();
 		for (int i = 0; i < sLength; i++) {
