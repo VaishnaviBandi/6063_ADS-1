ddd73ef7744494d17c416a35046c072021c8e0f9
VaishnaviBandi <vaishnavibandi9@msitprogram.net>
2018-09-29 17:39:57 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignments/m5/5.2 Josephus Problem/Solution.java
+++ assignments/m5/5.2 Josephus Problem/Solution.java
@@ -1,14 +1,33 @@
 import java.util.Scanner;
+/**
+ *the class for applying circular queue.
+ */
 class CircularQueue {
+    /**
+     * the object for operations class.
+     */
     private Operations obj;
+    /** constructor to initialize object.
+    */
     CircularQueue() {
         obj = new Operations();
     }
+    /**
+     *the method is to push the elements.
+     *to queue.
+     * @param      number  The number
+     */
     public void push(final int number) {
         for (int i = 0; i < number; i++) {
             obj.insertEnd(i);
         }
     }
+    /**
+     * the method to perform circular.
+     *queue
+     * @param      number  The number
+     * @param      length  The length
+     */
     public void pop(final int number, final int length) {
         int[] arr = new int[length];
         Node temp = obj.head;
@@ -33,9 +52,20 @@ class CircularQueue {
         System.out.println(arr[length - 1]);
     }
 }
+/**
+ *the class for main method.
+ */
 final class Solution {
+    /**
+     *an empty constructor.
+     */
     private Solution() {
     }
+    /**
+     *the main method to give user input.
+     *
+     * @param  args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         CircularQueue obj = new CircularQueue();
