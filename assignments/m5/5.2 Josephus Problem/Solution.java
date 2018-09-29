d42473518b1a9dc86381a32e6092d1fc63109a35
VaishnaviBandi <vaishnavibandi9@msitprogram.net>
2018-09-29 17:38:18 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ assignments/m5/5.2 Josephus Problem/Solution.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+class CircularQueue {
+    private Operations obj;
+    CircularQueue() {
+        obj = new Operations();
+    }
+    public void push(final int number) {
+        for (int i = 0; i < number; i++) {
+            obj.insertEnd(i);
+        }
+    }
+    public void pop(final int number, final int length) {
+        int[] arr = new int[length];
+        Node temp = obj.head;
+        int range = number;
+        int counter = 0;
+        int index = 0;
+        while (obj.size != 0) {
+            while (counter != range - 1) {
+                obj.insertEnd(temp.data);
+                temp = temp.next;
+                obj.deleteStart();
+                counter++;
+            }
+            arr[index++] = temp.data;
+            temp = temp.next;
+            counter = 0;
+            obj.deleteStart();
+        }
+        for (int i = 0; i < length - 1; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println(arr[length - 1]);
+    }
+}
+final class Solution {
+    private Solution() {
+    }
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        CircularQueue obj = new CircularQueue();
+        int lines = scan.nextInt();
+        scan.nextLine();
+        while (scan.hasNext()) {
+            String line = scan.nextLine();
+            String[] tokens = line.split(" ");
+            obj.push(Integer.parseInt(tokens[0]));
+            obj.pop(Integer.parseInt(tokens[1]),
+                    Integer.parseInt(tokens[0]));
+        }
+    }
+}
