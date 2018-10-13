74c39884d7782a89ae0e1e040fb13fc1dfc11c9e
VaishnaviBandi <vaishnavibandi9@msitprogram.net>
2018-10-13 13:09:45 +0530

    "check_style_score -> 2 of 5 passed. style score: 0/1 "

--- assignments/m18/Week-3_Assignment-1/Solution.java
+++ assignments/m18/Week-3_Assignment-1/Solution.java
@@ -157,7 +157,7 @@ class Solution {
                     System.out.println(sa.stk[k].getName() + " "
                                        + sa.stk[k].getStock());
                 }
-                // System.out.println();
+                System.out.println();
                 int arrsize = sa.getsize();
                 for (int k = arrsize - 1; k >= arrsize - 5; k--) {
                     System.out.println(sa.stk[k].getName() + " "
