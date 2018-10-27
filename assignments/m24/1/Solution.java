cc2623e0bb7a5063045d7e733f2fa741eeb118c1
VaishnaviBandi <vaishnavibandi9@msitprogram.net>
2018-10-27 12:15:14 +0530

    "m24_a1_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m24/1/Solution.java
+++ assignments/m24/1/Solution.java
@@ -10,10 +10,12 @@ class Solution {
         int studentData = sc.nextInt();
         // System.out.println("studentData");
         sc.nextLine();
-        //System.out.println(data data data);
+        // System.out.println(data data data);
+        
         SeparateChainingHashST<Integer, Student> schain = new SeparateChainingHashST<>();
         Student s = new Student();
         int i = 0;
+
         while (i < studentData) {
             String strng = sc.nextLine();
             String darray[] = strng.split(",");
@@ -22,24 +24,34 @@ class Solution {
             schain.put(Integer.parseInt(darray[0]), s);
             i++;
         }
+
         int j = sc.nextInt();
         sc.nextLine();
         int k = 0;
         while (k < j) {
+
             // System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+
             String karray[] = sc.nextLine().split(" ");
             if (karray[2].equals("1")) {
+
                 // System.out.println("ifff");
+
                 int key = Integer.parseInt(karray[1]);
                 if (schain.contains(key)) {
                     System.out.println(schain.get(key).stuName);
+
                     // System.out.println("@@@@@@@@@@@@");
+
                 } else {
                     System.out.println("Student doesn't exists...");
                 }
             }
             if (karray[2].equals("2")) {
+
                 // System.out.println("nono@@@@@@@@@@@@");
+
                 int key = Integer.parseInt(karray[1]);
                 if (schain.contains(key)) {
                     System.out.println(schain.get(key).stuMarks);
